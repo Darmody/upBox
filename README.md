eea2c0014f42a0e876e423b8682e17e8dde446fd
Cai Huanyu <eterlf41@gmail.com>
2014-04-09 09:54:37 +0800

    v1.1Beta 	2014-4-9 09:47
    v1.1Beta  2014-4-9 09:47

--- README.md
+++ README.md
@@ -9,24 +9,6 @@ upBox
 由知乎某相关问题“知乎回答问题编辑框用 Ctrl+V 粘贴图片是如何实现的？”及喻小伟小朋友的提议决定，<br/>
 implement 一个在 edit box 中直接 paste picture 即可 upload 的jQuery plugin！
 
-当前版本 version
-================
-
-v1.0Beta 	2014-4-3 18:46
--------------------------------------------------------------
-
-1. only support screenshot 产生的图片！
-2. 仅 support 可支持 HTML5 的 explorer！
-3. support 将 picture insert into edit box！
-4. don't support 将插入的 picture upload！
-5. 似乎只 support <div> 标签！（具体请看“举个栗子”环节）
-6. edit box 的 style 非常ugly！
-
-历史记录 history
-================
-
-(none)
-
 举个栗子 example
 ================
 
@@ -43,25 +25,38 @@ jQuery support
 				<title>编辑框CTRL-V粘贴图片栗子</title>
 
 				<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-				<script type="text/javascript" src="upBox.js"></script>
-				<script type="text/javascript" src="pastePic.js"></script>
-		
-		
+				<script type="text/javascript" src="upBox.js"></script>	
 			</head>
 		<body>
 		
-					<div class="edit-box">
-				
-					</div>
+			<div class="edit-box">
+			</div>
 		</body>
 		</html>
 
-### js code
 
-		$(document).ready(function(){
+当前版本 version
+================
 
-			$(".edit-box").upBox();
+v1.1Beta 	2014-4-9 09:47
+-------------------------------------------------------------
 
-		});
+1. support 将插入的 screenshot upload 至 server（注意：在服务器端处理时，应取name="file"的值，如：$_FILES['file']）
+2. 添加 example.html
+3. don't support Firefox
+4. 图片不按比例 zoom
+5. 悄悄地少少地美化了下编辑框
 
 
+历史记录 history
+================
+
+v1.0Beta 	2014-4-3 18:46
+-------------------------------------------------------------
+
+1. only support screenshot 产生的图片！
+2. 仅 support 可支持 HTML5 的 explorer！
+3. support 将 picture insert into edit box！
+4. don't support 将插入的 picture upload！
+5. 似乎只 support /<div/> 标签！（具体请看“举个栗子”环节）
+6. edit box 的 style 非常ugly！
--- upBox/upBox.js
+++ upBox/upBox.js
@@ -1,12 +1,29 @@
 (function($){
 
-  $.fn.upBox = function() { 	
+  $.fn.upBox = function(options) { 	
+
+    //参数
+    var defaults = {    
+
+      width : '35%',                //编辑框宽度
+      height : '240px',             //编辑框高度
+      border : '1px dashed grey',    //编辑框边框
+
+      uploadUrl : ''                //处理ajax上传的路径
+    };    
+    var opts = $.extend(defaults, options); 
+
   	//主体功能
   	this.each(function(){ 
   		//获取当前元素
   		var _this = $(this);
   		//将div设置成可编辑状态
   		_this.attr("contentEditable", "true");
+
+      //设置样式
+      _this.attr("style", "width:" + opts.width + ";"
+                        + "height:" + opts.height + ";" 
+                        + "border:" + opts.border + ";");
 
   		//监听编辑框粘贴事件
   		_this.on('paste', function(e) {
@@ -17,23 +34,66 @@
     			//逐个处理
     			for(var i = 0; i < items.length; i++) {
 
-    				var file = items[i].getAsFile();
+    				var file = items[i].getAsFile();  //获取文件
 
-    				reader = new FileReader();
+            var img = new Image();
 
- 	  				reader.onload = function(e){
-        
-    	    			var img = new Image();
-    	    			//获取图片文件的路径
-    	    			img.src = e.target.result;
-    	    			//插入到编辑框中
-    	    			_this.append(img);
-    				};
-    				//读取文件
-    				reader.readAsDataURL(file);
+            //获取图片文件的路径
+            fileHandler(file, img);
+
+            //图像缩放
+            img.width = 200;
+            img.height = 200;
+            
+            
+            //将图片插入编辑框
+            _this.append(img);
     			}
     		}
   		});
   	}); 
+
+    function fileHandler(file, img) {
+            //读取文件
+            var fileReader = new FileReader();
+ 
+            fileReader.readAsDataURL(file);
+            //创建xmlHttpRequest对象
+            var xmlHttpRequest = new XMLHttpRequest();
+            //xmlHttpRequest open操作
+            xmlHttpRequest.open("POST", options.uploadUrl, true);
+            //针对Firefox
+            if (file.getAsBinary) {
+
+            //     var data = dashes + boundary + crlf +
+            //         "Content-Disposition: form-data;" +
+            //         "name=\"" + "test" + "\";" +
+            //         "filename=\"" + unescape(encodeURIComponent(file.name)) + "\"" + crlf +
+            //         "Content-Type: application/octet-stream" + crlf + crlf +
+            //         file.getAsBinary() + crlf +
+            //         dashes + boundary + dashes 
+
+            //     xmlHttpRequest.setRequestHeader("Content-Type", "multipart/form-data;boundary=" + boundary);
+         
+                xmlHttpRequest.sendAsBinary(data);
+            //针对Chrome
+            } else if (window.FormData) { // Chrome
+                //获取数据
+                var formData = new FormData();
+                //将文件加入数据
+                formData.append("file", file);
+                //发送
+                xmlHttpRequest.send(formData);
+                //监听发送状态
+                xmlHttpRequest.onreadystatechange = function(){
+　　　　　　      //发送成功
+                  if ( xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200 ) {
+                    //获取文件在服务器的保存地址
+                    img.src = xmlHttpRequest.responseText;
+　　　　　　      
+                  }
+　　　　       };
+            }
+        }
   };
 })( jQuery );
