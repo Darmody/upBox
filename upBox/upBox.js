ec660681eb1844f2155b9148578c39999a196f59
Cai Huanyu <eterlf41@gmail.com>
2014-08-02 11:54:38 +0800

    return img

--- upBox/upBox.js
+++ upBox/upBox.js
@@ -1,6 +1,6 @@
 (function($){
 
-  $.fn.upBox = function(options) { 	
+  $.fn.upBox = function(options) {  
 
     //参数
     var defaults = {    
@@ -13,45 +13,46 @@
     };    
     var opts = $.extend(defaults, options); 
 
-  	//主体功能
-  	this.each(function(){ 
-  		//获取当前元素
-  		var _this = $(this);
-  		//将div设置成可编辑状态
-  		_this.attr("contentEditable", "true");
+    //主体功能
+    this.each(function(){ 
+      //获取当前元素
+      var _this = $(this);
+      //将div设置成可编辑状态
+      _this.attr("contentEditable", "true");
 
       //设置样式
       _this.attr("style", "width:" + opts.width + ";"
                         + "height:" + opts.height + ";" 
                         + "border:" + opts.border + ";");
 
-  		//监听编辑框粘贴事件
-  		_this.on('paste', function(e) {
-  			//判断剪切板中Files类型的对象是否存在
-			if(e.originalEvent.clipboardData.getData('Files') != undefined) {
-    			//获取剪切板内容中的对象
-    			var items = e.originalEvent.clipboardData.items;
-    			//逐个处理
-    			for(var i = 0; i < items.length; i++) {
+      var img = new Image();
+      //监听编辑框粘贴事件
+      _this.on('paste', function(e) {
+        //判断剪切板中Files类型的对象是否存在
+      if(e.originalEvent.clipboardData.getData('Files') != undefined) {
+          //获取剪切板内容中的对象
+          var items = e.originalEvent.clipboardData.items;
+          //逐个处理
+          for(var i = 0; i < items.length; i++) {
 
-    				var file = items[i].getAsFile();  //获取文件
-
-            var img = new Image();
+            var file = items[i].getAsFile();  //获取文件
 
             //获取图片文件的路径
             fileHandler(file, img);
 
             //图像缩放
-            img.width = 200;
-            img.height = 200;
+            img.width = 50;
+            img.height = 50;
             
             
             //将图片插入编辑框
             _this.append(img);
-    			}
-    		}
-  		});
-  	}); 
+          }
+        }
+      });
+
+      return img;
+    }); 
 
     function fileHandler(file, img) {
             //读取文件
